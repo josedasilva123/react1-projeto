7c10c9623afa206d9a6b3455d5affab8621221c2
josedasilva123 <alex.v.conder@gmail.com>
2024-07-05 14:28:45 -0300

    recipes list data

---
+++ src/data/recipes.data.ts
@@ -0,0 +1,9 @@
+import Pizza1 from "../assets/Pizza1.jpg";
+import Pizza2 from "../assets/Pizza2.jpg";
+import Pizza3 from "../assets/Pizza3.jpg";
+
+export const recipeList = [
+    { id: 1, name: "Quatro queijos", price: 39, img: Pizza1 },
+    { id: 2, name: "Vegana", price: 44, img: Pizza2 },
+    { id: 2, name: "Marguerita", price: 39, img: Pizza3 },
+];
