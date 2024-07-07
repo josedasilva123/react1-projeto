40fded83e3a1b4e74578531a08b9568fec171bcb
josedasilva123 <alex.v.conder@gmail.com>
2024-07-06 23:01:59 -0300

    styles header

--- src/components/template/Header/index.tsx
+++ src/components/template/Header/index.tsx
@@ -1,29 +1,32 @@
 import Logo from "../../../assets/PizzaLogo.svg";
+import styles from "./style.module.css";
 
 export function Header() {
    return (
-      <header className="background-white">
+      <header className={`background-white ${styles.header}`}>
          <div className="container">
-            <img src={Logo} alt="Logo Pizza Planet" />
-            <nav>
-               <ul>
-                  <li>
-                     <a className="menu-item" href="#sabores">
-                        Sabores
-                     </a>
-                  </li>
-                  <li>
-                     <a className="menu-item" href="#sobre">
-                        Sobre
-                     </a>
-                  </li>
-                  <li>
-                     <a className="button red" href="#formulario">
-                        Reservar agora
-                     </a>
-                  </li>
-               </ul>
-            </nav>
+            <div className={styles.box}>
+               <img src={Logo} alt="Logo Pizza Planet" />
+               <nav>
+                  <ul>
+                     <li>
+                        <a className="menu-item" href="#sabores">
+                           Sabores
+                        </a>
+                     </li>
+                     <li>
+                        <a className="menu-item" href="#sobre">
+                           Sobre
+                        </a>
+                     </li>
+                     <li>
+                        <a className="button red" href="#formulario">
+                           Reservar agora
+                        </a>
+                     </li>
+                  </ul>
+               </nav>
+            </div>
          </div>
       </header>
    );
