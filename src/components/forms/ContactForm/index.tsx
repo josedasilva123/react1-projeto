b715eb9273a822bb062fb3258fbe7e55ec7577b4
josedasilva123 <alex.v.conder@gmail.com>
2024-07-06 21:04:14 -0300

    styles form fields

--- src/components/forms/ContactForm/index.tsx
+++ src/components/forms/ContactForm/index.tsx
@@ -1,8 +1,8 @@
 export function ContactForm(){
     return(
         <form>
-            <input type="text" name="name" id="name" placeholder="Nome" />
-            <input type="email" name="email" id="email" placeholder="E-mail" />
+            <input className="field" type="text" name="name" id="name" placeholder="Nome" />
+            <input className="field" type="email" name="email" id="email" placeholder="E-mail" />
             <button className="button red" type="submit">Enviar mensagem</button>
         </form>
     )
