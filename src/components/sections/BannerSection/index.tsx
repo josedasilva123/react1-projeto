503617905cd2b79a514e266d355372ecf1e2719c
josedasilva123 <alex.v.conder@gmail.com>
2024-07-06 23:02:32 -0300

    styles banner section

--- src/components/sections/BannerSection/index.tsx
+++ src/components/sections/BannerSection/index.tsx
@@ -1,13 +1,19 @@
 import Banner from "../../../assets/BannerImage.jpg";
+import styles from "./style.module.css";
 
 export function BannerSection() {
    return (
       <section className="background-yellow">
          <div className="container">
-            <h1 className="title one">A melhor pizza da cidade</h1>
+            <div className={styles.textBox}>
+               <h1 className="title one">A melhor pizza da cidade</h1>
+            </div>
          </div>
 
-         <img src={Banner} alt="Pizza de lombo." />
+         <div className={styles.imageBox}>
+            <img src={Banner} alt="Pizza de lombo." />
+         </div>
+
          <div className="divisor"></div>
       </section>
    );
