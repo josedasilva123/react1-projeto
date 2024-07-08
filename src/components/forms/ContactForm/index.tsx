198f03052bee7a108e2f7eadccda214bcad4be77
josedasilva123 <alex.v.conder@gmail.com>
2024-07-08 19:23:02 -0300

    styles form section

--- src/components/forms/ContactForm/index.tsx
+++ src/components/forms/ContactForm/index.tsx
@@ -1,6 +1,8 @@
+import styles from "./style.module.css";
+
 export function ContactForm(){
     return(
-        <form>
+        <form className={styles.form}>
             <input className="field" type="text" name="name" id="name" placeholder="Nome" />
             <input className="field" type="email" name="email" id="email" placeholder="E-mail" />
             <button className="button red" type="submit">Enviar mensagem</button>
--- src/components/sections/FormSection/index.tsx
+++ src/components/sections/FormSection/index.tsx
@@ -1,8 +1,9 @@
 import { ContactForm } from "../../forms/ContactForm";
+import styles from "./style.module.css";
 
 export function FormSection() {
    return (
-      <section id="formulario" className="background-yellow">
+      <section id="formulario" className={`background-yellow section-p ${styles.section}`}>
          <div className="container sm">
             <h2 className="title one center">Formulário de contato</h2>
             <p className="text center">
