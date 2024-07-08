232d8282f64a13c98a93ce830df17ec056f38035
josedasilva123 <alex.v.conder@gmail.com>
2024-07-08 19:16:27 -0300

    styles cta section

--- src/components/sections/CtaSection/index.tsx
+++ src/components/sections/CtaSection/index.tsx
@@ -1,16 +1,17 @@
 import CtaImage from "../../../assets/CtaImage.jpg";
+import styles from "./style.module.css";
 
 export function CtaSection() {
    return (
       <section>
-         <div className="background-yellow">
-            <h3 className="title three">Conheça a melhor pizza da cidade!</h3>
+         <div className={`background-yellow ${styles.headline}`}>
+            <h3 className="title three center">Conheça a melhor pizza da cidade!</h3>
          </div>
-         <div>
-            <div>
+         <div className={styles.box}>
+            <div className={styles.imageBox}>
                <img src={CtaImage} alt="Pizza marguerita" />
             </div>
-            <div className="background-red">
+            <div className={`background-red ${styles.contentBox}`}>
                <h2 className="title one center white">Faça uma reserva</h2>
                <p className="text center white">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut mi
